46b54b139f6133f1f493e65514cb73da160e6312
liaihong <478701610@qq.com>
2019-03-29 10:55:03 +0800

    add test

--- TestDemo/TestDemo/ViewController.swift
+++ TestDemo/TestDemo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let view = UIView()
+        view.backgroundColor = UIColor.purple
+        self.view.addSubview(view)
+        view.frame = CGRect(x: 0, y: 80, width: 80, height: 40)
     }
 
 
